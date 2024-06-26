013e5459fd74990d86dfc077ec9e43f4dddfa106
QQSuperDogGaming <75622557+QQSuperDogGaming@users.noreply.github.com>
2024-06-26 10:19:08 +0800

    Update main.js

--- main.js
+++ main.js
@@ -10,9 +10,9 @@ window.addEventListener('load', () => {
             this.load.image('leaderboardButton', 'assets/leaderboardButton.png');
             this.load.image('backButton', 'assets/backButton.png');
             this.load.image('menuBackground', 'assets/menuBackground.png');
-            this.load.image('character1', 'assets/character1.png');
-            this.load.image('character2', 'assets/character2.png');
-            this.load.image('character3', 'assets/character3.png');
+            this.load.image('character1Button', 'assets/character1Button.png');
+            this.load.image('character2Button', 'assets/character2Button.png');
+            this.load.image('character3Button', 'assets/character3Button.png');
             this.load.image('map1', 'assets/map1.png');
             this.load.image('map2', 'assets/map2.png');
             this.load.image('leaderboardBackground', 'assets/leaderboardBackground.png'); // Preload leaderboard background
@@ -26,9 +26,9 @@ window.addEventListener('load', () => {
             this.fullscreenButton = this.add.image(this.scale.width - 40, 40, 'fullscreen').setInteractive().setDisplaySize(32, 32);
             this.leaderboardButton = this.add.image(this.scale.width / 2, this.scale.height / 2 + 100, 'leaderboardButton').setInteractive().setDisplaySize(200, 80);
 
-            this.character1 = this.add.image(this.scale.width / 2 - 100, this.scale.height / 2 - 200, 'character1').setInteractive().setDisplaySize(50, 50);
-            this.character2 = this.add.image(this.scale.width / 2, this.scale.height / 2 - 200, 'character2').setInteractive().setDisplaySize(50, 50);
-            this.character3 = this.add.image(this.scale.width / 2 + 100, this.scale.height / 2 - 200, 'character3').setInteractive().setDisplaySize(50, 50);
+            this.character1 = this.add.image(this.scale.width / 2 - 100, this.scale.height / 2 - 200, 'character1Button').setInteractive().setDisplaySize(50, 50);
+            this.character2 = this.add.image(this.scale.width / 2, this.scale.height / 2 - 200, 'character2Button').setInteractive().setDisplaySize(50, 50);
+            this.character3 = this.add.image(this.scale.width / 2 + 100, this.scale.height / 2 - 200, 'character3Button').setInteractive().setDisplaySize(50, 50);
             this.map1 = this.add.image(this.scale.width / 2 - 100, this.scale.height / 2 - 100, 'map1').setInteractive().setDisplaySize(50, 50);
             this.map2 = this.add.image(this.scale.width / 2, this.scale.height / 2 - 100, 'map2').setInteractive().setDisplaySize(50, 50);
 
